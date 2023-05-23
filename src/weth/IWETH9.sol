ddf56af60b720ed16427d8186319094c894cbd1e
B7 <bytes32@outlook.com>
2023-05-23 21:28:09 +0800

    fork lib

---
+++ src/weth/IWETH9.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+// ███████╗████████╗███████╗ █████╗ ██████╗ ███╗   ██╗
+// ██╔════╝╚══██╔══╝██╔════╝██╔══██╗██╔══██╗████╗  ██║
+// ███████╗   ██║   █████╗  ███████║██████╔╝██╔██╗ ██║
+// ╚════██║   ██║   ██╔══╝  ██╔══██║██╔══██╗██║╚██╗██║
+// ███████║   ██║   ███████╗██║  ██║██║  ██║██║ ╚████║
+// ╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+pragma solidity ^0.8.0;
+
+import "forge-std/interfaces/IERC20.sol";
+
+interface IWETH9 is IERC20 {
+    event Deposit(address indexed dst, uint256 wad);
+    event Withdrawal(address indexed src, uint256 wad);
+
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+}
